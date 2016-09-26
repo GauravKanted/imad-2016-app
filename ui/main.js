62b69e3ac1aa99f457951e16aa6213e8027bfd70
GauravKanted <gkanted@gmail.com>
2016-09-26 19:31:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var mtext = document.getElementById('main-text');
+var moveimg = document.getElementById('madi');
+
+moveimg.onClick= function (){
+    moveimg.style.marginLeft = '100px' ;
+    
+};
+
+element.innerHTML = 'New Values!';
