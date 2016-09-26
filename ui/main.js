661163a7266ce69f6d4afa51072bbaa24356c8c9
GauravKanted <gkanted@gmail.com>
2016-09-26 20:52:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,13 @@ console.log('Loaded!');
 var mtext = document.getElementById('main-text');
 mtext.innerHTML = 'New Values!';
 
-var marginLeft=0;
+//var marginLeft=0;
 
 var moveimg = document.getElementById('madi');
 
 moveimg.onClick = function () {
     moveimg.style.marginLeft = '100px';
-    var interval = setInterval(moveRight,100); 
+    //var interval = setInterval(moveRight,100); 
 };
 
 function moveRight() {
