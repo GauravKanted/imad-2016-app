db24fa08c5325b6e1ccfb6d60ccc340aeab487b5
GauravKanted <gkanted@gmail.com>
2016-10-01 19:03:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,13 +23,40 @@ var name = nameInput.Value;
 var submit = document.getElementById("submitb");
 
 submit.onclick = function () {
-var names = ['name1','name2','name3','name4'];
-var list = '';
-for( i=0;i<names.length;i++)
-{
-    list  += '<li>' + names[i] +'</li>' ;
-}
+    
+var request = new XMLHttpRequest();
+    request.onreadystatechange = function () 
+    {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) { 
+            var counter= request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString(); 
+         }
+      }
+    };
+    
+    request.open('GET','http://gauravkanted.imad.hasura-app.io/counter', true);
+    request.send(null);var request = new XMLHttpRequest();
+    request.onreadystatechange = function () 
+    {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) { 
+                            var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for( i=0;i<names.length;i++)
+                {
+                    list  += '<li>' + names[i] +'</li>' ;
+                }
+                
+                var ul= document.getElementById('namelist');
+                ul.innerHTML= list; 
+         }
+      }
+    };
+    
+    request.open('GET','http://gauravkanted.imad.hasura-app.io/submit-name', true);
+    request.send(null);
 
-var ul= document.getElementById('namelist');
-ul.innerHTML= list;
+
 };
