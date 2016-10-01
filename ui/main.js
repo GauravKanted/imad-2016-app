91695c3857d09a794f62f500dad96eb751cfc5a1
GauravKanted <gkanted@gmail.com>
2016-10-01 17:29:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,9 @@ moveimg.onclick = function () {
     var interval = setInterval(moveRight, 100); 
 };
 
+var button = document.getElementById('button');
+button.onclick = function() {
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
