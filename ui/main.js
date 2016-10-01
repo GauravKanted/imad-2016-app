efb37a06abc9c47cd7f6c12d6864116c11ad6889
GauravKanted <gkanted@gmail.com>
2016-10-01 15:06:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,18 +3,17 @@ var mtext = document.getElementById('main-text');
 
 mtext.innerHTML = 'New Values Loaded!';
 
-//var marginLeft=0;
-
 var moveimg = document.getElementById('madi');
-/*
-moveimg.onClick = function () {
-    moveimg.style.marginLeft = '100px';
-    //var interval = setInterval(moveRight,100); 
-};
-*/
+var marginLeft = 0;
 
 function moveRight() {
-    //marginLeft = marginLeft+10;
-    moveimg.style.marginLeft= marginLeft + "px";
+    marginLeft = marginLeft+10;
+    moveimg.style.marginLeft= marginLeft + 'px';
 }
+
+
+moveimg.onClick = function () {
+   // moveimg.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100); 
+};
 
