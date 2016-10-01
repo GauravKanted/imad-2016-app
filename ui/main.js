513fb125e8da097d3fe35cfd154eeff4374bd257
GauravKanted <gkanted@gmail.com>
2016-10-01 19:14:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,12 +24,8 @@ var submit = document.getElementById("submitb");
 
 submit.onclick = function () {
     
-var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
    
-    };
-    
-    request.open('GET','http://gauravkanted.imad.hasura-app.io/counter', true);
-    request.send(null);var request = new XMLHttpRequest();
     request.onreadystatechange = function () 
     {
         if(request.readyState === XMLHttpRequest.DONE) {
