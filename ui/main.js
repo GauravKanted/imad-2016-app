74bec52be1638ed3ed0fbda14c9e74997306a717
GauravKanted <gkanted@gmail.com>
2016-10-01 17:31:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ moveimg.onclick = function () {
 
 var button = document.getElementById('counter');
 button.onclick = function() {
-    counter = counter+1;
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
