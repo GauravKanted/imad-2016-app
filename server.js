b278687c203881994f55906a6d9cd720d8e2f4e4
Gaurav Kanted <gkanted@gmail.com>
2016-11-22 18:52:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/css/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', './css/style.css'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
