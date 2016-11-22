9a4cb5029d8379f5badb35fa94bd3731d96598bd
Gaurav Kanted <gkanted@gmail.com>
2016-11-22 22:13:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
@@ -462,12 +462,11 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+*/
 
 
 
-
-
-/*var express = require('express');
+var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
