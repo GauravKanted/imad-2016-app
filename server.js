90a96616b2d830c46ccfd0d8a29a747cbe269703
Gaurav Kanted <gkanted@gmail.com>
2016-11-23 09:41:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ function createTemplate (data) {
                             </a>
                         </li>
                         <li>
-                            <a href="https://github.com/aniketaditya">
+                            <a href="https://github.com/gauravkanted">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-github fa-stack-1x fa-inverse"></i>
