dca680883d69e1f04fd4f243d88c260443563817
Gaurav Kanted <gkanted@gmail.com>
2016-11-24 21:57:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -350,7 +350,7 @@ app.get('/', function (req, res) {
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
-
+/*
 app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
@@ -449,6 +449,7 @@ app.get('/fonts/LobsterTwo-Regular.ttf', function (req, res) {
   app.get('/vendor/font-awesome/fonts/fontawesome-webfont.woff2', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/fonts' , 'OpenSans-Regular.ttf'));
 });
+*/
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
